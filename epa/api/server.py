86f4b06a2f16343f37ec4b2b4337cd1c54c65d95
PolinaBohaichuk <polinicus@gmail.com>
2024-11-29 23:57:58 +0200

    Upgrade file uploader

--- epa/api/server.py
+++ epa/api/server.py
@@ -1,22 +1,26 @@
 # Import flask and datetime module for showing date and time
-from io import BytesIO
-from flask import Flask, request, jsonify
-import datetime
+import os
 import pickle
+import datetime
+from io import BytesIO
+from werkzeug.utils import secure_filename
+from flask import Flask, request, jsonify, redirect, url_for
 
 x = datetime.datetime.now()
 
 with open('../../ml-models/classification/serialized/decisionTreeModel.pkl', 'rb') as f:
     model = pickle.load(f)
 
+ALLOWED_EXTENSIONS = {'csv', 'xml'}
+
 # Initializing flask app
 app = Flask(__name__)
+
 
 
 # Route for seeing a data
 @app.route('/data')
 def get_time():
-
     # Returning an api for showing in  reactjs
     return {
         'Name':"tesm", 
@@ -25,18 +29,25 @@ def get_time():
         "programming":"python"
     }
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/data_analysis', methods=['POST'])
 def upload_file():
-    """Handles the upload of a file."""
+    #Handles the upload of a file
     d = {}
     try:
         file = request.files['file_from_react']
-        filename = file.filename
-        print(f"Uploading file {filename}")
-        file_bytes = file.read()
-        file_content = BytesIO(file_bytes).readlines()
-        print(file_content)
-        d['status'] = 1
+        if file.filename == '':
+            print('No selected file')
+            d['status'] = 0
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            os.makedirs(os.path.join(app.instance_path), exist_ok=True)
+            file.save(os.path.join(app.instance_path, filename))
+            print(f"Uploading file {filename}")
+            d['status'] = 1
+            #return redirect(url_for('download_file', name=filename))
 
     except Exception as e:
         print(f"Couldn't upload file {e}")
